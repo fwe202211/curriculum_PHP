cdfbc4beb24a6e3f76cac7295d809de67f616bf0
fwe202211 <fwe202211@gmail.com>
2023-04-06 12:11:30 +0900

    練習問題2

---
+++ PHPjob/3-3/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Practice2</title>
+</head>
+<body>
+  <!-- ①フォームを作成しましょう -->
+  <form action="result.php" method="get">
+  <!-- ここにフォームを記述してください -->
+    好きな名前を入れてください : <input type="text" name="name">
+    <br>
+    1~6の中で好きな数字を入れてください : <input type="number" name="number">
+    <br>
+    <input type="submit" value="送信">
+  </form>
+</body>
+</html>
---
+++ PHPjob/3-3/result.php
@@ -0,0 +1,29 @@
+<?php
+  //②フォームからのデータを受け取ります
+$my_name = $_GET["name"];
+$my_number = $_GET["number"];
+
+  //③受け取った数字に1~6までのランダムな数字を掛け合わせて変数に入れてください
+$number = $my_number*mt_rand(1,6);
+
+  //④掛け合わせた数字の結果によっておみくじを選び、変数に入れます
+if($number <= 10){
+  $luck = "凶";
+}elseif(11 <= $number && $number <= 15){
+  $luck = "小吉";
+}elseif(16 <= $number && $number <= 20){
+  $luck = "中吉";
+}elseif(21 <= $number && $number <= 25){
+  $luck = "吉";
+}elseif(26 <= $number && $number <= 35){
+  $luck = "大吉";
+}else{
+  $luck = "残念";
+}
+
+  //⑤今日の日付と、名前、番号、おみくじ結果を表示しましょう
+echo date("Y-m-d H:i:s", time());
+echo "<br>";
+echo "名前は${my_name}です。<br>";
+echo "番号は${number}です。<br>";
+echo "結果は${luck}です。";
